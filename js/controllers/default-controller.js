51bc0b67c55f49c8549231db3ad62a9db939e102
Andrew Rigney <andrew@therigneygroup.com.au>
2021-06-06 08:55:05 +1000

    IIFE as arrow function

--- js/controllers/default-controller.js
+++ js/controllers/default-controller.js
@@ -2,8 +2,8 @@ var pageController = {
     init: function () { 
         //Load model
         //declare an IIFE as the callback to process the model data
-        dotmin.loadModel("default", `(function() {
-                defaultModel.greeting += "!";
+        dotmin.loadModel("default", `(() => {
+                defaultModel.greeting += "! :)";
                 document.getElementById("slide1_h1").textContent = defaultModel.greeting;
                 document.getElementById("slide1_p").textContent = defaultModel.copy;
                 document.getElementById("slide1_cta").textContent = defaultModel.cta;
