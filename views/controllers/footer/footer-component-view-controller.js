16677fd7a6ffb3f491fa593c1ad8366a4c990d49
Andrew Rigney <andrew@therigneygroup.com.au>
2021-06-07 19:41:29 +1000

    Sample Bootstrap 5 toast component

--- views/controllers/footer/footer-component-view-controller.js
+++ views/controllers/footer/footer-component-view-controller.js
@@ -1,7 +1,13 @@
 var footerComponentViewController = {
     init: function () {
-        dotmin.on("#trg", "click", function(e) { 
-            window.alert("dotmin - miniscule JavaScript app framework! Brought to you by The Rigney Group"); 
+        dotmin.on("#trg", "click", function (e) {
+            console.info("dotmin - miniscule JavaScript app framework! Brought to you by The Rigney Group");
+            var toastElList = [].slice.call(document.querySelectorAll('.toast'))
+            var toastList = toastElList.map(function (toastEl) {
+                return new bootstrap.Toast(toastEl);
+            });
+            toastList.forEach(toast => toast.show());
+            console.log(toastList);
         });
     }
 };
