5c0dab3e7a3cf39775b8be0ed5c3cab823e5cd52
Andrew Rigney <andrew@therigneygroup.com.au>
2021-06-07 19:49:19 +1000

    Footer

--- views/controllers/footer/footer-component-view-controller.js
+++ views/controllers/footer/footer-component-view-controller.js
@@ -1,13 +1,15 @@
 var footerComponentViewController = {
     init: function () {
-        dotmin.on("#trg", "click", function (e) {
-            console.info("dotmin - miniscule JavaScript app framework! Brought to you by The Rigney Group");
-            var toastElList = [].slice.call(document.querySelectorAll('.toast'))
-            var toastList = toastElList.map(function (toastEl) {
-                return new bootstrap.Toast(toastEl);
+        dotmin.ready( function() {
+            dotmin.on("#trg", "click", function (e) {
+                console.info("dotmin - miniscule JavaScript app framework! Brought to you by The Rigney Group");
+                var toastElList = [].slice.call(document.querySelectorAll('.toast'))
+                var toastList = toastElList.map(function (toastEl) {
+                    return new bootstrap.Toast(toastEl);
+                });
+                toastList.forEach(toast => toast.show());
+                console.log(toastList);
             });
-            toastList.forEach(toast => toast.show());
-            console.log(toastList);
-        });
+        });        
     }
 };
