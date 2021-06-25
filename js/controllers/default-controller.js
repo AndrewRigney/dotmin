67d033c80cb8d9c891a7684bd91d05c40ded6de8
Andrew Rigney <andrew@therigneygroup.com.au>
2021-06-25 19:42:15 +1000

    Lighthouse LCP and CLS optimisation

--- js/controllers/default-controller.js
+++ js/controllers/default-controller.js
@@ -2,7 +2,7 @@
 _m.lm().then(() => {
     pageModel.greeting += "! :)";
         document.getElementById("slide1_h1").textContent = pageModel.greeting;
-        document.getElementById("slide1_p").textContent = pageModel.copy;
+        document.getElementById("slide1_p").innerHTML = pageModel.copy;
         document.getElementById("slide1_cta").textContent = pageModel.cta;
         document.getElementById("slide1_cta").setAttribute("href", pageModel.ctaLink);
 });
@@ -10,7 +10,7 @@ _m.lm().then(() => {
 fetch("https://api.ipify.org/?format=json")
     .then(response => response.json())
     .then(data => {
-        document.getElementById("slide1_p").innerHTML += "<br/>You're visiting us from " + data.ip + ". Remember, there's no place like 127.0.0.1!";
+        document.getElementById("ipAddress").innerHTML = data.ip 
     }).catch(error => {
         console.error(error);
     });
